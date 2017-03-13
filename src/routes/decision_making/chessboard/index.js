89c57f36e8f49462c0ac8cd2cfcf559a76a7b364
ss707494 <ss707494@163.com>
2017-03-13 15:25:46 +0800

    添加decision

--- src/routes/decision_making/chessboard/index.js
+++ src/routes/decision_making/chessboard/index.js
@@ -6,12 +6,27 @@ import './style.scss'
 import {handleClassForAsync} from '../../../store/reducers'
 import {name, reducer, actions} from './action';
 
+import _ from 'lodash'
 class Chessboard extends Component {
+
+  componentWillMount() {
+    /**
+     * qsort []     = []
+     * qsort (x:xs) = qsort (filter (< x) xs) ++ [x] ++ qsort (filter (>= x) xs)
+     */
+    const qsort = arr => arr.length < 2
+        ? arr
+        : qsort(_.filter(_.drop(arr), (e) => e < arr[0])).concat(arr[0]).concat(qsort(_.filter(_.drop(arr), (e) => e >= arr[0])))
+
+    let a = [1, 3, 4, 5, 6, 7, 8, 9, 1];
+    const qsort2 = qsort(a);
+    console.log(qsort2);
+
+  }
 
   render() {
     return <div>
       <span styleName="box">
-      <img styleName="imgBox" src="http://cdn.iciba.com/news/2017/0308/20170308111545764.jpg"/>
       </span>
     </div>
   }
